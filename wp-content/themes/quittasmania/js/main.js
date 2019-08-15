cc1e8cab3d074fb2fa0936d1cc847eb182d20722
alrex <alrex@webhelper.com.au>
2019-08-15 16:49:52 +0800

    Implement global Heading that has animation.

--- wp-content/themes/quittasmania/js/main.js
+++ wp-content/themes/quittasmania/js/main.js
@@ -141,5 +141,5 @@ jQuery(document).ready(function($){
     
     sr.reveal('.content-title .title-small', { duration: 800, delay: 10, origin: 'right', distance: '80px', opacity: 0, scale: 1 });
     sr.reveal('.content-title .with-underline', { duration: 2000, delay: 80, origin: 'right', distance: '100px', opacity: 0, scale: 1 });
-    sr.reveal('.content-title .underline', { duration: 1500, delay: 90, origin: 'left', distance: '800px', opacity: 0, scale: 1 });
+    sr.reveal('.content-title .underline', { duration: 1500, delay: 90, origin: 'left', distance: '600px', opacity: 0, scale: 1 });
 });
